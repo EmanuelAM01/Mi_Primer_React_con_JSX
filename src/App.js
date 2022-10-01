26d605ca62022334a8bb3ecea7c74f24df4fcfd6
Emanuel Aguirre Molina <emagui11572@gmail.com>
2022-10-01 17:46:27 -0500

    Mi Primer React

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Lista from './Componentes/Lista/Lista';
+
+
+function App() {
+  return (
+    <div className="App">
+        <h1>Hello Dojo!</h1>
+        <Lista/>
+    </div>
+  );
+}
+
+export default App;
